eb4fa9da7086c3cae525bda82168a5019ee736c1
cptSummer <mrsummer2008@gmail.com>
2022-01-23 17:52:30 +0300

    hy

---
+++ Home task 7/src/ICollection.java
@@ -0,0 +1,11 @@
+public interface ICollection {
+    boolean add(int index, String value);
+
+    boolean add(String value);
+
+    boolean delete(int index);
+
+    boolean delete(String value);
+
+    String get(int index);
+}
