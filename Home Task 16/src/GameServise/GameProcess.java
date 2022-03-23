4c121ccc59d0fefa3ac7e54fcd27b7bbc3e89d79
cptSummer <mrsummer2008@gmail.com>
2022-03-23 23:43:50 +0300

    ht 16

--- Home Task 16/src/GameServise/GameProcess.java
+++ Home Task 16/src/GameServise/GameProcess.java
@@ -10,9 +10,8 @@ public class GameProcess {
     public void startGame(Player player) {
         Scanner sc = new Scanner(System.in);
         int i = 0;
-        System.out.println(player.getGameRound());
         while (i != player.getGameRound()) {
-            player.setGameRound(i++);
+            i++;
             int userElement = 0;
             int aiElement = 0;
             byte checkAns = 0;
@@ -23,50 +22,48 @@ public class GameProcess {
                 userElement = sc.nextByte();
             } while (!List.of(1, 2, 3).contains(userElement));
 
-            aiElement = (byte) (Math.random() * 3);
-
-            System.out.println("Your element = " + elements[userElement - 1] + "\nAI element = " + elements[aiElement]);
-
-            String condition = matrixWLT(userElement - 1, aiElement);
-            System.out.println(condition);
-            growNum(condition, player);
+            aiElement = new AiService().aiMove();
 
 
-            System.out.println("Continue?\ncontinue-1 end-2");
-            checkAns = sc.nextByte();
-            if (checkAns == 2) {
-                player.setGameRound(i);
-                System.out.println("end");
-                break;
+            System.out.println(matrixWLT(userElement - 1, aiElement));
+            growNum(userElement-1,aiElement,player);
+
+
+            if (i != player.getGameRound()) {
+                System.out.println("Continue?\ncontinue-1 end-2");
+                checkAns = sc.nextByte();
+                if (checkAns == 2) {
+                    player.setGameRound(i);
+                    System.out.println("end");
+                    break;
+                }
             }
-            System.out.println(i);
-            System.out.println(player.getGameRound());
         }
         System.out.println("_______________________________________");
         statistic.endGame(player);
     }
 
     private String matrixWLT(int userElement, int aiElement) {
-        String[][] matrix = new String[3][3];
-        matrix[0][0] = "Tie";
-        matrix[0][1] = "Louse";
-        matrix[0][2] = "Win";
-        matrix[1][0] = "Win";
-        matrix[1][1] = "Tie";
-        matrix[1][2] = "Louse";
-        matrix[2][0] = "Louse";
-        matrix[2][1] = "Win";
-        matrix[2][2] = "Tie";
+        String[][] matrix = {
+            {" Computer --> rock, \n Player --> rock \n Computer == Player",
+                    " Computer --> paper, \n Player --> rock \n Player win!",
+                    " Computer --> scissors, \n Player --> rock \n Player win!"},
 
+            {" Computer --> rock, \n Player --> paper \n Computer win!",
+                    " Computer --> rock, \n Player --> rock \n Computer == Player",
+                    " Computer --> scissors, \n Player --> paper \n Computer win!"},
+
+            {" Computer --> rock, \n Player --> scissors \n Computer win!",
+                    " Computer --> paper, \n Player --> scissors \n Player win!",
+                    " Computer --> rock, \n Player --> rock \n Computer == Player"}
+        };
         return matrix[userElement][aiElement];
     }
 
-    private void growNum(String condition, Player player) {
-        switch (condition) {
-            case "Tie" -> player.setTieRounds(player.getTieRounds() + 1);
-            case "Win" -> player.setUserWin(player.getUserWin() + 1);
-            case "Louse" -> player.setUserLouse(player.getUserLouse() + 1);
-        }
+    private void growNum(int user, int ai, Player player) {
+        if(user == ai) player.setTieRounds(player.getTieRounds()+1);
+        if((user==0 && ai == 1) || (user==2 && ai == 0) || (user==1 && ai ==2)){ player.setUserLouse(player.getUserLouse()+1);}
+        if((ai==0 && user == 1) || (ai==1 && user ==2)|| (user==0 && ai == 2)){player.setUserWin(player.getUserWin()+1);}
     }
 
 }
--- Home Task 16/src/GameTest/GameTest.java
+++ Home Task 16/src/GameTest/GameTest.java
@@ -1,5 +1,19 @@
 package GameTest;
 
+import GameServise.AiService;
+import  org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GameTest {
+
+    @RepeatedTest(1000)
+    void aiTest(){
+        int result = new AiService().aiMove();
+        System.out.println(result);
+        assertTrue(result >= 0);
+        assertTrue(result <= 2);
+    }
 
 }
